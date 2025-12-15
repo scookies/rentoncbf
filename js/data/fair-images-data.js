83d692a5e473ce924dc5b1f6b3d003fd6448fa06
Tricia Lee <tricial03@gmail.com>
2025-12-14 22:26:06 -0800

    Add compressed fair images (web-optimized)

--- js/data/fair-images-data.js
+++ js/data/fair-images-data.js
@@ -7,16 +7,15 @@ window.FairImagesData = {
       "images/midWinterFair/CBF_3989.jpeg"
     ],
     "summerFair": [
-      "images/summerFair/CBF_6510.jpeg",
-      "images/summerFair/CBF_6538.jpeg",
       "images/summerFair/CBF_6555.jpeg",
-      "images/summerFair/CBF_6567.jpeg"
+      "images/summerFair/CBF_6567.jpeg",
+      "images/summerFair/CBF_6571.jpeg",
+      "images/summerFair/CBF_6582.jpeg"
     ],
     "preHolidayMarket": [
       "images/preHolidayMarket/CBF_7389.jpeg",
       "images/preHolidayMarket/CBF_7392.jpeg",
-      "images/preHolidayMarket/CBF_7398.jpeg",
-      "images/preHolidayMarket/IMG_9248.jpeg"
+      "images/preHolidayMarket/CBF_7398.jpeg"
     ]
   }
 };
--- js/modules/fair-carousel.js
+++ js/modules/fair-carousel.js
@@ -32,7 +32,7 @@ class FairCarouselModule {
 
     async getExistingImages(folderName) {
         // Final fallback - hardcoded image map
-                const imageMap = {
+                        const imageMap = {
             'midWinterFair': [
                 'images/midWinterFair/CBF_3966.jpeg',
                 'images/midWinterFair/CBF_3967.jpeg',
@@ -40,16 +40,15 @@ class FairCarouselModule {
                 'images/midWinterFair/CBF_3989.jpeg'
             ],
             'summerFair': [
-                'images/summerFair/CBF_6510.jpeg',
-                'images/summerFair/CBF_6538.jpeg',
                 'images/summerFair/CBF_6555.jpeg',
-                'images/summerFair/CBF_6567.jpeg'
+                'images/summerFair/CBF_6567.jpeg',
+                'images/summerFair/CBF_6571.jpeg',
+                'images/summerFair/CBF_6582.jpeg'
             ],
             'preHolidayMarket': [
                 'images/preHolidayMarket/CBF_7389.jpeg',
                 'images/preHolidayMarket/CBF_7392.jpeg',
-                'images/preHolidayMarket/CBF_7398.jpeg',
-                'images/preHolidayMarket/IMG_9248.jpeg'
+                'images/preHolidayMarket/CBF_7398.jpeg'
             ]
         };
 
